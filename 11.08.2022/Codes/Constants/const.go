d8cc5acc580d528afe747c1dce4868633a487d3d
NJavokhir <nematovjavohir58@gmail.com>
2022-11-08 20:20:29 +0500

    added some code files

---
+++ 11.08.2022/Codes/Constants/const.go
@@ -0,0 +1,21 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+const (
+	a = 12
+	b = 13.56
+	c = "hello"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+}
