4ab2cbebd648bf21c7dedc6a472f99c4759f56d3
NJavokhir <nematovjavohir58@gmail.com>
2023-04-25 23:12:50 +0500

    sets up server

---
+++ Web-Authentication-Encryption-JWT/golang-arch/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type person struct {
+	First string
+}
+
+func main() {
+	// p1 := person{
+	// 	First: "Javokhir",
+	// }
+
+	// p2 := person{
+	// 	First: "Ravi",
+	// }
+
+	// xp := []person{p1, p2}
+
+	// bs, err := json.Marshal(xp)
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("PRINT JSON", string(bs))
+
+	// xp2 := []person{}
+
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("back into a Go data structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
+}
