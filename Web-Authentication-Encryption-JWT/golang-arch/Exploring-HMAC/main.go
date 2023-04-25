e780f5591a52e557e2ca0d3e0e5f2ff0a3b1d4f0
NJavokhir <nematovjavohir58@gmail.com>
2023-04-26 02:11:12 +0500

    Hmac functions

--- Web-Authentication-Encryption-JWT/golang-arch/Exploring-HMAC/main.go
+++ Web-Authentication-Encryption-JWT/golang-arch/Exploring-HMAC/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+var key = []byte{}
+
 func main() {
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
 	pass := "123456789"
 
 	hashedPass, err := hashPassword(pass)
@@ -40,4 +47,24 @@ func comparePassword(password string, hashedPass []byte) error {
 	return nil
 }
 
-// Continued on "Exploring-HMAC" folder
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key)
+
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("error in signMessage while hashing message: %w", err)
+	}
+
+	signature := h.Sum(nil)
+	return signature, nil
+}
+
+func checkSig(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("error in checking while getting signature of message: %w", err)
+	}
+
+	same := hmac.Equal(newSig, sig)
+	return same, nil
+}
