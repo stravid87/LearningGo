7361f6d8381ded8cf596ea8e27e27218d55158c1
NJavokhir <nematovjavohir58@gmail.com>
2023-05-06 14:24:01 +0500

    added sha256 file

---
+++ Web-Authentication-Encryption-JWT/golang-arch/S8-Exploring-encryption/sha256.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("sample-file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+
+	_, err = io.Copy(h, f)
+	if err != nil {
+		log.Fatalln("couldn't io.copy")
+	}
+
+	fmt.Printf("here's the type BEFORE Sum: %T\n", h)
+	fmt.Printf("%v\n", h)
+	xb := h.Sum(nil)
+	fmt.Printf("here's the type AFTER Sum: %T\n", xb)
+	fmt.Printf("%x\n", xb)
+
+	xb = h.Sum(nil)
+	fmt.Printf("here's the type AFTER SECOND Sum: %T\n", xb)
+	fmt.Printf("%x\n", xb)
+
+	xb = h.Sum(xb)
+	fmt.Printf("here's the type AFTER THIRD Sum and passing in xb: %T\n", xb)
+	fmt.Printf("%x\n", xb)
+}
